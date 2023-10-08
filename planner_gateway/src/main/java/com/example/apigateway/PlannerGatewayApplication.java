5c06969ef3b58f1f645ecf8d88b87d0371d1e66f
gantedil <dim22xxx@gmail.com>
2023-10-08 05:03:21 +0300

    init project and add entity module

---
+++ planner_gateway/src/main/java/com/example/apigateway/PlannerGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class PlannerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerGatewayApplication.class, args);
+    }
+
+}
---
+++ planner_server/src/test/java/com/example/planner_server/PlannerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planner_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
